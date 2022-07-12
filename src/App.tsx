ad6670c431927a3a01c5313851ddd41f41f9fc53
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-12 15:49:59 -0300

    refactor(context): muda importação de contextos para main
    segrega todos os contextos no arquivo main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { ClientProvider } from './contexts/client/ClientContext';
 import { Router } from './routes/Router';
 
 function App() {
   return (
-    <ClientProvider>
-      <Router />
-    </ClientProvider>
+    <Router />
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './contexts/auth/AuthContext';
+import { ClientProvider } from './contexts/client/ClientContext';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <ClientProvider>
+            <App />
+          </ClientProvider>
+        </AuthProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
