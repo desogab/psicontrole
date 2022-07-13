94b3f1c61e5f614b4261af8d4bacb708ba21569c
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-13 20:00:36 -0300

    refactor(activeClient): adiciona um toggle para ativar e desativar usuario
    Adiciona um toggle com um alerta de confirmação para desativar e ativar cliente

--- src/pages/client/ClientById.tsx
+++ src/pages/client/ClientById.tsx
@@ -4,8 +4,17 @@ import {
   Heading,
   SimpleGrid,
   Spacer,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Switch,
+  useDisclosure,
+  Button,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AddressCard } from '../../components/ClientCards/AddressCard';
 import { EmergencyCard } from '../../components/ClientCards/EmergencyCard';
@@ -16,13 +25,33 @@ import { ClientInfo } from '../../types';
 export function ClientById() {
   const { id } = useParams();
   const { getClientById } = useClient();
+  const { onOpen, isOpen, onClose } = useDisclosure();
   const [client, setClient] = useState<ClientInfo>();
+  const [isActive, setIsActive] = useState<ClientInfo['active']>(false);
+
+  const cancelRef = useRef(null);
+
+  async function toggleActive() {
+    await fetch(`http://localhost:3001/clients/${id}`, {
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      method: 'PUT',
+      body: JSON.stringify({
+        ...client,
+        active: !isActive,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) setIsActive(!isActive);
+      })
+      .finally(() => onClose());
+  }
 
   useEffect(() => {
     if (id !== undefined) {
       (async () => {
         const clientById = await getClientById(id);
         setClient(clientById);
+        setIsActive(clientById.active);
       })();
     }
   }, [id, getClientById]);
@@ -32,12 +61,44 @@ export function ClientById() {
       <Flex alignItems="center" mb="4">
         <Heading as="h1">{client?.name}</Heading>
         <Spacer />
-        {client?.active ? (
-          <Badge variant="solid" colorScheme="green">ativo</Badge>
-        ) : (
-          <Badge variant="solid" colorScheme="red">inativo</Badge>
-        )}
+        {/* {isActive ? ( */}
+        {/*   <Badge variant="solid" colorScheme="green">ativo</Badge> */}
+        {/* ) : ( */}
+        {/*   <Badge variant="solid" colorScheme="red">inativo</Badge> */}
+        {/* )} */}
+        <Switch isChecked={isActive} colorScheme={isActive ? 'green' : 'gray'} onChange={onOpen} />
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+          isCentered
+          size="sm"
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                {isActive ? 'Desativar Paciente' : 'Ativar Paciente'}
+              </AlertDialogHeader>
 
+              <AlertDialogBody>
+                Tem certeza que deseja fazer isso?
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancelar
+                </Button>
+                <Button
+                  colorScheme={isActive ? 'red' : 'green'}
+                  onClick={() => toggleActive()}
+                  ml={3}
+                >
+                  {isActive ? 'Desativar' : 'Ativar'}
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </Flex>
       <SimpleGrid spacing={3}>
         {client && <PersonalCard client={client} />}
